17496b771c3c1a78b9c9c4182b54c20798812ca6
dharikamal <76258348+dharikamal@users.noreply.github.com>
2021-02-03 19:53:31 +0300

    Create funtions.js

---
+++ funtions.js
@@ -0,0 +1,39 @@
+const A = ["dhari", "may", "nor", "dalal"];
+
+//console.log(A[0])
+//console.log(A[1])
+//console.log(A[2])
+//console.log(A[3])
+//console.log(A.length);
+
+//A.push("Taiba");
+//console.log(A)
+//console.log(A.length)
+
+//const lastone = A.pop();
+//console.log(A.length)
+//console.log(A)
+//console.log(lastone)
+
+//console.log(A.slice(0,2));
+//console.log(A)
+
+//console.log(A.slice(1,-1));
+//console(A)
+//A.slice(-2, A.length)
+//console.log(A);
+//let count = 0;
+//while (count < 12){
+  //  console.log(count);
+    //count ++;
+//}
+
+let i = 0;
+while ( i < A.length){
+    console.log(`Hi, I'm ${A[i]}`);
+    i++
+}
+
+for (let i = 0; i< A.length; i++){
+    console.log(`Hi, I'm ${A[i]}`);
+}
